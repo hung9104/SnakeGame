7905736420a41c43aaec19a5598c2ce2bdf09ed0
hung9104 <119866817+hung9104@users.noreply.github.com>
2023-06-14 20:18:39 +0700

    Add files via upload

---
+++ SnakeGame/Canvas.js
@@ -0,0 +1,12 @@
+const canvas = document.getElementById("canvas");
+const block =  20
+const game_size = 600;
+canvas.width = game_size;
+canvas.height = game_size;
+const ctx = canvas.getContext('2d');
+ctx.fillStyle = 'black';
+ctx.fillRect(0, 0, game_size, game_size);
+
+
+
+
